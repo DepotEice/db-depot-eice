fc5e835577a29f80ef87188a9e3ac938f7fffefb
Samad Hatsijev <s.hatsijev@hotmail.com>
2022-09-01 20:31:24 +0200

    fix! usertokens_approve
    doesn't want to leave

--- DB.DepotEice/dbo/StoredProcedures/UserTokens/spUserTokens_Approve.sql
+++ DB.DepotEice/dbo/StoredProcedures/UserTokens/spUserTokens_Approve.sql
@@ -1,19 +1,37 @@
-﻿CREATE PROCEDURE [dbo].[spUserTokens_Approve]
-	@id UNIQUEIDENTIFIER,
-	@type NVARCHAR(100),
-	@userSecurityStamp UNIQUEIDENTIFIER
+﻿--CREATE PROCEDURE [dbo].[spUserTokens_Approve]
+--	@id UNIQUEIDENTIFIER,
+--	@type NVARCHAR(100),
+--	@userSecurityStamp UNIQUEIDENTIFIER
+--AS
+--BEGIN
+--	SET NOCOUNT ON;
+
+--	DECLARE @value NVARCHAR(32) = (SELECT [dbo].[UserTokens].[Value] 
+--									FROM [dbo].[UserTokens] 
+--									WHERE [dbo].[UserTokens].Id = @id);
+
+--	IF @expirationDate < GETDATE()
+--		RETURN 0;										
+--	ELSE IF @value = [dbo].[fnCreateUserTokenValue](@userSecurityStamp, @type)
+--		SELECT 1;
+--	ELSE
+--		SELECT 0;
+--END;
+
+CREATE PROCEDURE [dbo].[spUserTokens_Approve]
+  @id UNIQUEIDENTIFIER,
+  @type NVARCHAR(100),
+  @userSecurityStamp UNIQUEIDENTIFIER
 AS
 BEGIN
-	SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-	DECLARE @value NVARCHAR(32) = (SELECT [dbo].[UserTokens].[Value] 
-									FROM [dbo].[UserTokens] 
-									WHERE [dbo].[UserTokens].Id = @id);
+  DECLARE @value NVARCHAR(32) = (SELECT [dbo].[UserTokens].[Value] 
+                  FROM [dbo].[UserTokens] 
+                  WHERE [dbo].[UserTokens].Id = @id);
 
-	IF @expirationDate < GETDATE()
-		RETURN 0;										
-	ELSE IF @value = [dbo].[fnCreateUserTokenValue](@userSecurityStamp, @type)
-		SELECT 1;
-	ELSE
-		SELECT 0;
+  IF @value = [dbo].[fnCreateUserTokenValue](@userSecurityStamp, @type)
+    RETURN 1;
+  ELSE
+    RETURN 0;
 END;
