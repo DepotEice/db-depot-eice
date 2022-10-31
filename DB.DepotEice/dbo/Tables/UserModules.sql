1b6b36e179b3e88e754f93103653f1b41f7ee9e8
Soultan Hatsijev <soultan_98@hotmail.com>
2022-10-31 15:15:27 +0100

    Added on delete cascade for Module reference

--- DB.DepotEice/dbo/Tables/UserModules.sql
+++ DB.DepotEice/dbo/Tables/UserModules.sql
@@ -5,5 +5,5 @@
 	[IsAccepted] BIT NULL,
 	CONSTRAINT [PK_UserModules] PRIMARY KEY ([UserId], [ModuleId]),
 	CONSTRAINT [FK_UserModules_User] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]),
-	CONSTRAINT [FK_UserModules_Module] FOREIGN KEY ([ModuleId]) REFERENCES [Modules]([Id])
+	CONSTRAINT [FK_UserModules_Module] FOREIGN KEY ([ModuleId]) REFERENCES [Modules]([Id]) ON DELETE CASCADE
 )
