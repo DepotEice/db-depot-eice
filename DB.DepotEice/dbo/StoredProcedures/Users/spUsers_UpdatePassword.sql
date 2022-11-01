8d291ef1edcb57ccbfc375640eaaa306551b18d0
Soultan Hatsijev <soultan_98@hotmail.com>
2022-11-01 14:14:20 +0100

    Created store procedure to update user's password

---
+++ DB.DepotEice/dbo/StoredProcedures/Users/spUsers_UpdatePassword.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_UpdatePassword]
+	@userId UNIQUEIDENTIFIER,
+	@password NVARCHAR(50),
+	@salt NVARCHAR(MAX)
+AS
+	UPDATE [dbo].[Users]
+	SET
+		[PasswordHash] = [dbo].[fnHashPassword](@password, @salt),
+		[ConcurrencyStamp] = NEWID(),
+		[SecurityStamp] = NEWID()
+	WHERE [Id] = @userId
+GO
