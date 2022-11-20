80e86c17711b0efc7ef5affbb2fa2295ec2085d3
Soultan Hatsijev <soultan_98@hotmail.com>
2022-11-20 15:39:17 +0100

    isAccepted default value 0

--- DB.DepotEice/dbo/Tables/UserModules.sql
+++ DB.DepotEice/dbo/Tables/UserModules.sql
@@ -2,7 +2,7 @@
 (
 	[UserId] UNIQUEIDENTIFIER NOT NULL,
 	[ModuleId] INT NOT NULL,
-	[IsAccepted] BIT NULL,
+	[IsAccepted] BIT NOT NULL DEFAULT 0,
 	CONSTRAINT [PK_UserModules] PRIMARY KEY ([UserId], [ModuleId]),
 	CONSTRAINT [FK_UserModules_User] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]),
 	CONSTRAINT [FK_UserModules_Module] FOREIGN KEY ([ModuleId]) REFERENCES [Modules]([Id]) ON DELETE CASCADE
