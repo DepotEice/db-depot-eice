3a9751aad7ad9bc35215d8472029284e0b51c7b8
Soultan Hatsijev <soultan_98@hotmail.com>
2023-06-26 14:41:32 +0200

    added File

---
+++ DB.DepotEice/dbo/StoredProcedures/Files/spFiles_Create.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[spFiles_Create]
+	@key NVARCHAR(255),
+	@path NVARCHAR(255),
+	@size INT,
+	@type NVARCHAR(255)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[Files]
+	(
+		[Key],
+		[Path],
+		[Size],
+		[Type]
+	)
+	VALUES
+	(
+		@key,
+		@path,
+		@size,
+		@type
+	);
+
+	SELECT SCOPE_IDENTITY();
+END
---
+++ DB.DepotEice/dbo/Tables/Files.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [dbo].[Files]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Key] NVARCHAR(255) NOT NULL,
+	[Path] NVARCHAR(255) NULL,
+	[Size] INT NULL,
+	[Type] NVARCHAR(255) NOT NULL,
+	[CreatedAt] DATETIME2 NOT NULL DEFAULT GETDATE(), 
+	[UpdatedAt] DATETIME2 NULL, 
+	[DeletedAt] DATETIME2 NULL
+)
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_File_Update]
+    ON [dbo].[Files]
+    AFTER UPDATE
+AS
+BEGIN
+   UPDATE [dbo].[Files]
+   SET [UpdatedAt] = GETDATE()
+   FROM Inserted i
+   WHERE [dbo].[Files].[Id] = i.Id
+END
+
+GO
+
+--CREATE TRIGGER [dbo].[Trigger_File_Delete]
+--	ON [dbo].[Files]
+--	INSTEAD OF DELETE
+--AS
+--BEGIN    
+--	UPDATE [dbo].[Files]
+--	SET
+--		[Key] = NULL,
+--		[Path] = NULL,
+--		[Size] = NULL,
+--		[Type] = NULL,
+--		[UpdatedAt] = GETDATE(),
+--		[DeletedAt] = GETDATE()
+--	FROM deleted d
+--	WHERE [dbo].[Files].[Id] = d.Id
+--END
