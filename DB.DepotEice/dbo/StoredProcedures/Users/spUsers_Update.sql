1c103e795af6a7c06c5d82cf33f0e57b4e8c63f5
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-01 19:29:03 +0200

    feat: new properties

--- DB.DepotEice/dbo/StoredProcedures/Users/spUsers_Update.sql
+++ DB.DepotEice/dbo/StoredProcedures/Users/spUsers_Update.sql
@@ -2,13 +2,19 @@
 	@id UNIQUEIDENTIFIER,
 	@firstName NVARCHAR(50),
 	@lastName NVARCHAR(100),
-	@birthDate DATE
+	@birthDate DATETIME,
+	@mobileNumber NVARCHAR(15),
+	@phoneNumber NVARCHAR(15),
+	@gender NVARCHAR(10)
 AS
 	UPDATE [dbo].[Users]
 	SET
 		[FirstName] = @firstName,
 		[Lastname] = @lastName,
 		[BirthDate] = @birthDate,
+		[MobileNumber] = @mobileNumber,
+		[PhoneNumber] = @phoneNumber,
+		[Gender] = @gender,
 		[ConcurrencyStamp] = NEWID()
 	WHERE [Id] = @id
 GO
