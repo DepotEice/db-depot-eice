f0d03e40e150417cd66067bcd040ce46384f5bf5
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-19 23:14:18 +0200

    feat: added remaining properties in delete user and update

--- DB.DepotEice/dbo/Tables/Users.sql
+++ DB.DepotEice/dbo/Tables/Users.sql
@@ -32,7 +32,9 @@ CREATE TRIGGER [dbo].[Trigger_Users_Update]
 AS
 BEGIN
    UPDATE [dbo].[Users]
-   SET [UpdatedAt] = GETDATE()
+   SET 
+    [UpdatedAt] = GETDATE(),
+    [ConcurrencyStamp] = NEWID()
    FROM Inserted i
    WHERE [dbo].[Users].[Id] = i.Id
 END
@@ -46,11 +48,17 @@ AS
 BEGIN    
     UPDATE [dbo].[Users]
     SET
+        [ProfilePictureId] = NULL,
         [FirstName] = NULL, 
         [Lastname] = NULL, 
+        [Gender] = NULL,
         [BirthDate] = NULL,
-        [ConcurrencyStamp] = NULL,
+        [MobileNumber]= NULL,
+        [PhoneNumber] = NULL,
+        [IsActive] = 0,
+        [ConcurrencyStamp] = NEWID(),
         [SecurityStamp] = NEWID(),
+        [UpdatedAt] = GETDATE(),
         [DeletedAt] = GETDATE()
     FROM deleted d
     WHERE [dbo].[Users].[Id] = d.Id
